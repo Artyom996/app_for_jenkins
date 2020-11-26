d4a5ac21ccb845f221a56626e999916300576673
Artyom <artem96artem@gmail.com>
2020-11-26 21:06:03 +0300

    Kal'kulyator

--- dockerrus.py
+++ dockerrus.py
@@ -1,13 +1,20 @@
-a = int(input('Введи первое число: '));
-b = int(input('Введи второе число: '));
-c = input('Введи что нужно с ним сделать?: ');
+import time
 
-if c == '+':
-	print (a+b);
-elif c == '-':
-	print (a-b);
-elif c == '*':
-	print (a*b);
-else:
-	print (a/b);
+
+
+while True:
+	a = int(input('Введи первое число: '));
+	b = int(input('Введи второе число: '));
+	c = input('Введи что нужно с ним сделать?: ');
+
+	if c == '+':
+		print (a+b);
+	elif c == '-':
+		print (a-b);
+	elif c == '*':
+		print (a*b);
+	else:
+		print (a/b);
+	time.sleep(1)
+
 
